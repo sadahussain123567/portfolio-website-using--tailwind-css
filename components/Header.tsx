f7d8eb55f1d43eef26fba77e2c39d2d1b2cd3bb2
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2025-01-08 01:25:50 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,47 +1,59 @@
 "use client";
 import React, { useState } from "react";
-
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
+
+  const handleCloseMenu = () => {
+    setIsActive(false);
+  };
+
   return (
     <>
-      <header className="border-b border-gray-700 bg-gradient-to-r from-[#083a3e] to-[#06352f]">
+      <header className="border-b border-gray-700 bg-gradient-to-r from-[#083a3e] to-[#06352f] ">
         <div className="max-w-screen-xl m-auto p-4">
           <div className="flex justify-between items-center">
+            {/* Logo */}
             <div className="flex items-center">
               <Image src="/favicon.png" alt="logo" width={50} height={50} />
               <span>Saddam</span>
             </div>
-            <div className="absolute top-5 right-3 sm:hidden">
+
+            {/* Hamburger Menu */}
+            <div className="absolute top-7 right-3 sm:hidden">
               <Menu onClick={() => setIsActive(!isActive)} />
             </div>
+
+            {/* Mobile Menu */}
             {isActive && (
-              <div>
-                <ul className="sm:hidden flex flex-col justify-between items-center gap-8 absolute top-[50px] right-4 bg-gradient-to-r from-[#083a3e] to-[#06352f] p-4 rounded-md">
+              <div className="absolute top-14 left-0 w-full h-screen   bg-gradient-to-r from-[#083a3e] to-[#06352f] sm:hidden flex items-center justify-center">
+                <ul className="flex flex-col items-center  gap-4 p-4 text-white">
                   <li>
                     <Link
-                      className="text-white text-[1.2rem] hover:text-xl"
+                      className="text-white text-[1.2rem] font-extrabold hover:text-xl"
                       href="/"
+                      onClick={handleCloseMenu}
                     >
                       Home
                     </Link>
                   </li>
                   <li>
                     <Link
-                      className="text-white text-[1.2rem] hover:text-xl"
+                      className="text-white text-[1.2rem] font-extrabold hover:text-xl"
                       href="/about"
+                      onClick={handleCloseMenu}
                     >
                       About
                     </Link>
                   </li>
                   <li>
                     <Link
-                      className="text-white text-[1.2rem] hover:text-xl"
+                      className="text-white text-[1.2rem] font-extrabold hover:text-xl"
                       href="/contact"
+                      onClick={handleCloseMenu}
                     >
                       Contact
                     </Link>
@@ -49,11 +61,13 @@ const Header = () => {
                 </ul>
               </div>
             )}
+
+            {/* Desktop Menu */}
             <div className="hidden sm:flex">
               <ul className="flex justify-between items-center gap-16">
                 <li>
                   <Link
-                    className="text-white text-[1.2rem] hover:text-xl"
+                    className="text-white text-[1.2rem] font-medium hover:text-2xl"
                     href="/"
                   >
                     Home
@@ -61,7 +75,7 @@ const Header = () => {
                 </li>
                 <li>
                   <Link
-                    className="text-white text-[1.2rem] hover:text-xl"
+                    className="text-white text-[1.2rem] font-medium hover:text-2xl"
                     href="/about"
                   >
                     About
@@ -69,7 +83,7 @@ const Header = () => {
                 </li>
                 <li>
                   <Link
-                    className="text-white text-[1.2rem] hover:text-xl"
+                    className="text-white text-[1.2rem] font-medium hover:text-2xl"
                     href="/contact"
                   >
                     Contact
